c883b0fad4eaa34718d981c7101b3c8357442b91
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-21 16:41:25 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,13 +20,13 @@ const STORAG_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 
 const onPlays = function (e) {
   console.log(e.seconds);
@@ -39,17 +39,17 @@ const savedTime = localStorage.getItem(STORAG_KEY);
 
 player
   .setCurrentTime(savedTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+  // .then(function (seconds) {
+  //   // seconds = the actual time that the player seeked to
+  // })
+  // .catch(function (error) {
+  //   switch (error.name) {
+  //     case 'RangeError':
+  //       // the time was less than 0 or greater than the video’s duration
+  //       break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+  //     default:
+  //       // some other error occurred
+  //       break;
+  //   }
+  // });
