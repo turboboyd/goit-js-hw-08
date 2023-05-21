86b3fedcdb15a601a457c68bce8872e2909fdfca
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-21 16:37:49 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,54 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const STORAG_KEY = 'videoplayer-current-time';
 
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', throttle(timeUpdate, 1000));
+
+// function timeUpdate (e) {
+//   localStorage.setItem(STORAG_KEY, e.seconds);
+// };
+
+// const savedTime = localStorage.getItem(STORAG_KEY);
+// const time = parseFloat(savedTime);
+// player.setCurrentTime(time);
+
+
+
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(timeUpdate, 1000));
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-function timeUpdate (e) {
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlays = function (e) {
+  console.log(e.seconds);
   localStorage.setItem(STORAG_KEY, e.seconds);
 };
 
+player.on('timeupdate',throttle(onPlays, 1000));
+
 const savedTime = localStorage.getItem(STORAG_KEY);
-const time = parseFloat(savedTime);
-player.setCurrentTime(time);
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
