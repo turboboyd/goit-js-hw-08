10e42aa27e3a1bb99d7eaf6bfb00fd7391a005f5
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-21 15:20:00 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryMarkup = galleryItems
   .map(
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate (e) {
+  localStorage.setItem(STORAGE_KEY, e.seconds);
+};
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+const time = parseFloat(savedTime);
+player.setCurrentTime(time);
