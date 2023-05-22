3e3192cb0d5a0283cd4e929042c41d65bfc459ef
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-22 16:18:31 +0200

    Up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,70 +1,47 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const formEl = document.querySelector('.feedback-form');
-const textareaEl = document.querySelector('.feedback-form textarea');
 const inputEl = document.querySelector('.feedback-form input');
-
+const textareaEl = document.querySelector('.feedback-form textarea');
+const inputValue = inputEl.value;
+const textareaValue = textareaEl.value;
 
 let formData = {};
 
 
 formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onTextInput, 1000));
+formEl.addEventListener('input', throttle(onTextInput, 500));
 
-// populateTextarea();
+populateTextarea();
 
 
 function onTextInput(e) {
   formData[e.target.name] = e.target.value;
-  console.log(formData);
-  formData;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
 }
-
-
-
-
-
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+
+  if (inputValue === '' || textareaValue === '') {
+    alert('All fields must be hidden');
+  } else {
+    const user = {
+      email: inputValue,
+      message: textareaValue,
+    };
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function onTextInput(e) {
-//   formData[e.target.name] = e.target.value;
-//   console.log(formData);
-//   const formDataJSON = JSON.stringify(formData);
-//   localStorage.setItem(STORAGE_KEY, formDataJSON);
-// }
-
-// function populateTextarea() {
-//   // const data = JSON.parse(STORAGE_KEY)
-//   // console.log(data)
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-//   if (savedMessage) {
-
-//       const { email, message } = JSON.parse(savedMessage);
-//       inputEl.value = email;
-//       textareaEl.value = message;
-//       console.dir(formEl);
-//   }
-// }
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+      const { email, message } = JSON.parse(savedMessage);
+      inputEl.value = email;
+      textareaEl.value = message;
+      console.log(textareaEl);
+  }
+}
