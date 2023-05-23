b3d323ecb524641863ad142a68be114959e0dea8
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-23 11:14:22 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,13 +5,13 @@ const STORAGE_KEY1 = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
 
 const onСurrentTime = function (e) {
   localStorage.setItem(STORAGE_KEY1, e.seconds);
@@ -24,17 +24,17 @@ const sevedTime = localStorage.getItem(STORAGE_KEY1);
 
 player
   .setCurrentTime(sevedTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+  // .then(function (seconds) {
+  //   // seconds = the actual time that the player seeked to
+  // })
+  // .catch(function (error) {
+  //   switch (error.name) {
+  //     case 'RangeError':
+  //       // the time was less than 0 or greater than the video’s duration
+  //       break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+  //     default:
+  //       // some other error occurred
+  //       break;
+  //   }
+  // });
