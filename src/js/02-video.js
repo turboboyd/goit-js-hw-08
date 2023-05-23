e2209551dab89b55228f8260e6fb01e1d4232c91
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-23 11:34:05 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -24,17 +24,17 @@ const sevedTime = localStorage.getItem(STORAGE_KEY1);
 
 player
   .setCurrentTime(sevedTime)
-  // .then(function (seconds) {
-  //   // seconds = the actual time that the player seeked to
-  // })
-  // .catch(function (error) {
-  //   switch (error.name) {
-  //     case 'RangeError':
-  //       // the time was less than 0 or greater than the video’s duration
-  //       break;
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-  //     default:
-  //       // some other error occurred
-  //       break;
-  //   }
-  // });
+      default:
+        // some other error occurred
+        break;
+    }
+  });
